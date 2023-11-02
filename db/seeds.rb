b2ba0c96addc08ff0496a0d0055c6acce313aa1a
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-11-02 03:03:21 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+printf '====== Creating User ... '
+
+# genrerate a default user
+if (Rails.env.development? || Rails.env.test?) && User.count.zero?
+  User.create!(email: 'teste@teste.com', password: 'password',
+               password_confirmation: 'password')
+end
+printf "DONE! ======\n\n"
+
+printf '====== Creating proponentes ... '
+# generate 10 proponentes
+10.times do |_proponente|
+  Proponente.create!(
+    nome: Faker::Name.name,
+    cpf: Faker::IDNumber.brazilian_citizen_number,
+    data_nascimento: Faker::Date.birthday,
+    logradouro: Faker::Address.street_address,
+    numero: Faker::Address.building_number,
+    bairro: Faker::Address.community,
+    cidade: Faker::Address.city,
+    estado: Faker::Address.state_abbr,
+    cep: Faker::Address.zip_code,
+    telefone_pessoal: Faker::PhoneNumber.cell_phone,
+    telefone_referencia: Faker::PhoneNumber.cell_phone,
+    salario: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+  )
+end
+printf "DONE! ======\n\n"
+
+puts ''
+puts '#................................................#'
+puts '       acesse o app com esse dados        '
+puts '#................................................#'
+puts "user: #{User.first.email}"
+puts 'senha: password'
+puts '#................................................#'
+puts '#................................................#'
+puts ''
+puts ''
