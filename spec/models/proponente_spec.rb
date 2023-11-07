0621676d53f212aba5651ce4e1df3dcbcf1a3d15
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-11-06 23:21:42 -0300

    WIP tests

--- spec/models/proponente_spec.rb
+++ spec/models/proponente_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Proponente, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:nome) }
-    it { should validate_presence_of(:salario) }
-    it { should validate_numericality_of(:salario).is_greater_than(0) }
     it { should validate_presence_of(:cpf) }
+    it { should validate_presence_of(:salario) }
     it { should validate_uniqueness_of(:cpf) }
+    it { should validate_numericality_of(:salario).is_greater_than(0) }
 
     it 'should validate cpf format' do
       proponente = Proponente.new(cpf: '123.456.789-00')
       expect(proponente).to_not be_valid
-      expect(proponente.errors[:cpf]).to include('is invalid')
+      expect(proponente.errors[:cpf]).to include('não é válido')
     end
   end
 
@@ -20,7 +20,7 @@ RSpec.describe Proponente, type: :model do
 
     it 'should calculate desconto_inss' do
       proponente.calcular_desconto_inss
-      expect(proponente.desconto_inss).to eq(330.0)
+      expect(proponente.desconto_inss).to eq(281.637)
     end
   end
 end
